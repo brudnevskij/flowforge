c27d1771bd51fce785c550df6f4ec5e76df9fab6
Segfaulter <brudnevskij@users.noreply.github.com>
2026-04-05 20:25:17 +0300

    refactor: fixed import

--- crates/api/src/dto/orders.rs
+++ crates/api/src/dto/orders.rs
@@ -1,3 +1,7 @@
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
 #[derive(Debug, Deserialize)]
 pub struct CreateOrderRequest {
     pub customer_reference: String,
