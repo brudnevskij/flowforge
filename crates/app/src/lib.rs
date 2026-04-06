f4e1ff38c361339fdb54d790c80d5d97be15534a
Segfaulter <brudnevskij@users.noreply.github.com>
2026-04-06 23:55:24 +0300

    feat: added storage trait for order service

--- crates/app/src/lib.rs
+++ crates/app/src/lib.rs
@@ -1,3 +1,5 @@
 mod create_order;
 
-pub use create_order::{CreateOrderCommand, CreateOrderValidationError};
+pub use create_order::{
+    CreateOrderCommand, CreateOrderStore, CreateOrderStoreError, CreateOrderValidationError,
+};
