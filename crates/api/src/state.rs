37d2ae8fa7f07c896ec01d00adef231c7af77eed
Segfaulter <brudnevskij@users.noreply.github.com>
2026-04-07 17:09:06 +0300

    feat: added traits for services into the state

--- crates/api/src/state.rs
+++ crates/api/src/state.rs
@@ -1,9 +1,19 @@
+use app::{CreateOrderCommand, CreateOrderService, CreateOrderServiceError, CreateOrderStore};
+use domain::Order;
 use std::sync::Arc;
-
-use app::CreateOrderService;
-use infra::PostgresCreateOrderStore;
 
 #[derive(Clone)]
 pub struct ApiState {
-    pub create_order_service: Arc<CreateOrderService<PostgresCreateOrderStore>>,
+    pub create_order_service: Arc<dyn CreateOrderUseCase>,
+    pub readiness_service: Arc<dyn ReadinessCheckUseCase>,
+}
+
+#[async_trait::async_trait]
+pub trait CreateOrderUseCase: Send + Sync {
+    async fn execute(&self, cmd: CreateOrderCommand) -> Result<Order, CreateOrderServiceError>;
+}
+
+#[async_trait::async_trait]
+pub trait ReadinessCheckUseCase: Send + Sync {
+    async fn is_ready(&self) -> bool;
 }
